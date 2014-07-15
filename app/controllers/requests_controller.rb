36f739ee2ce210b50ba0dbf306f104d114594727
deeeki <deeeki@gmail.com>
2014-07-16 06:24:16 +0900

    Apply basic authentication

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,7 @@
 class RequestsController < ApplicationController
   before_action :set_request, only: [:show]
   skip_before_action :verify_authenticity_token, only: [:create]
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASS'] if Rails.env.production?
 
   def index
     @requests = Request.all
